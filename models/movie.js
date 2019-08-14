3c7672bd906b5f4e1676de562926b7df0e107f91
Samantha <sami.shackelford@gmail.com>
2019-08-14 14:55:34 -0700

    add defaults and requirments

--- models/movie.js
+++ models/movie.js
@@ -5,18 +5,29 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var movieSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true
+    },
     releaseYear: {
         type: Number, 
-        default: 2000
+        default: function() {
+            return new Date().getFullYear();
+        },
+        min: 1927
     },
-    mpaaRating: String,
+    mpaaRating: {
+        type: String,
+        enum: ['G', 'PG', 'PG-13', 'R']
+    },
     // you cannot specify a default for an array without mongoose middleware
     cast: [String],
     nowShowing: {
         type: Boolean,
         default: false
     }
+}, {
+    timestamps: true
 });
 
 // compile schema into a model and export it
